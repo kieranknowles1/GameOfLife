af853a46441e2edd1b5178bf2e1abc330473558f
c4072047 <c4072047@18SCOMPD172>
2024-10-15 11:34:04 +0100

    Discuss threading and determinism

--- GameOfLife/Experiment/Experiment.cpp
+++ GameOfLife/Experiment/Experiment.cpp
@@ -71,6 +71,18 @@ namespace GameOfLife::Experiment {
 		// between attempts
 		std::vector<std::thread> threads;
 
+		// We create mThreads threads, each of which will run the threadFunc function
+		// threadFunc gets a task, runs it, then loops until there is nothing left to do
+		// If the desired result is found, all remaining tasks are cancelled which causes threads to exit at the end of their current task
+		// This acts as a simple thread pool, where threads are reused between attempts but not between experiments
+		// ExperimentRunner has read-only access to outside data, so is inherently thread-safe as long as said data is not modified (which is the case here)
+		// Note that even something as simple as this has a race condition if multiple threads are running experiments that will succeed, as the first one to finish
+		// will be considered the winner, making the whole thing non-deterministic.
+
+		// Determinism is not a requirement here, so this is fine but it would be a concern if we needed stable results. For example:
+		// - the game Factoio is deterministic, in multiplayer all clients have a full copy of the game state and run the same simulation
+		//   if something was non-deterministic, the game would desync between clients.
+		// - Nix builds are supposed to be deterministic, if a build is not exactly reproducible then it would break the rule that a configuration is a pure function of its inputs
 		for (int i = 0; i < mParameters.mThreads; i++)
 		{
 			threads.emplace_back(threadFunc, this);
